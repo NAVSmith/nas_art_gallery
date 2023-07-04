53c902ccb7757c41e0edb527f2087f9310a4ccce
NAVSmith <56131958+NAVSmith@users.noreply.github.com>
2023-07-04 14:55:01 +0200

    add some style to the images of the art work

--- components/kunst_stuecke/kunst_stuecke.js
+++ components/kunst_stuecke/kunst_stuecke.js
@@ -1,27 +1,49 @@
+
+// third party react and next
 import { Fragment } from "react";
+// third party style
 import styled from "styled-components";
 
+// local imports 
 import { StuckPreview } from "../kunst_preview/kunst_preview.js";
 
 export function KunstStuecke({ stuecke }) {
     console.log(stuecke);
     return (
-        <ul>
+        <ArtList>
             {/* start iterating to create the art pieces  */}
             {stuecke &&
                 stuecke.map((stuck) => {
                     console.log(stuck.slug);
                     return (
-                        <li key={stuck.slug}>
+                        <ArtEntry key={stuck.slug}>
                             <StuckPreview
                                 title={stuck.name}
                                 artistName={stuck.artist}
                                 imageUrl={stuck.imageSource} 
                                 slug={stuck.slug}                               
                             />
-                        </li>
+                        </ArtEntry>
                     );
                 })}
-        </ul>
+        </ArtList>
     );
 }
+
+const ArtList = styled.ul`
+  list-style: none;
+  padding-left: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+  justify-content: space-around;
+  ;
+`;
+
+const ArtEntry = styled.li`
+    width: 25rem;
+    /* min-width: 10rem; */
+    height: 25rem;
+`
+
