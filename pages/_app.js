59581791bc237be14d8ab7356c16571564b327e5
NAVSmith <56131958+NAVSmith@users.noreply.github.com>
2023-07-04 12:42:59 +0200

    add the fetcher function and fetching the data in the app compentent and then down to the home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
+import { Fragment } from "react";
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+// fetcher from api
+async function fetcherFunc(...arg) {
+    console.log("start fetching the data");
+    const res = await fetch(...arg);
+    return await res.json();
+}
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
-  );
+    const { data, isLoading, error } = useSWR(
+        "https://example-apis.vercel.app/api/art",
+        fetcherFunc
+    );
+    console.log("the data ", data);
+    console.log("loading? ", isLoading);
+    console.log("the error:", error);
+    return (
+        <>
+            <GlobalStyle />
+            <Component
+                {...pageProps}
+                ArtStuecke={isLoading || error ? [] : data}
+            />
+        </>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function HomePage() {
+export default function HomePage({ArtStuecke}) {
+  console.log('respone form api' ,ArtStuecke);
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <p>this is the data</p>
     </div>
   );
 }
+
+/*
+Tasks
+
+Acceptance Criteria
+
+ All art pieces are displayed as a list 🖼️
+ Each art piece's image is displayed 🖼️
+ Each art piece's title is displayed 🖼️
+ Each art piece's artist is displayed 🖼️
+
+ Fetch all art pieces with SWR in pages/index.js
+ Create the component ArtPieces to render a list
+ ArtPieces props: pieces
+ Create the component ArtPiecePreview
+ ArtPiecePreview props: image, title, artist
+ All acceptance criteria marked with 🖼️ are covered by component testing
+
+*/
